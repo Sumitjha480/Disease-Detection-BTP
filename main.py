866ea5bc4f370f178ca819ac3c168b5189168e3a
Sumit <sumitjha480@gmail.com>
2022-09-27 15:38:25 +0530

    html

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-from flask import Flask,render_template
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from src import const, preprocess
+import os
+import shutil
+from pathlib import Path
+import json
+
+templates = Jinja2Templates(directory="./templates")
+
+app = FastAPI()
+
+# app.mount(
+#     "/static",
+#     StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+#     name="static",
+# )
 
 
-
-app = Flask(__name__)
-
-app.config["DEBUG"] = True
-
-@app.route('/')
-def home():
-    return render_template('template/index.html')
+@app.get('/')
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>Page Not Found</p>", 404
+@app.post('/predict')
+async def predict(image: UploadFile = File(...)):
+    temp_file = save_to_disk(image, path="temp", save_as='temp')
+    result = preprocess.predict(temp_file)
+    with open(const.diagnosis_dir + const.diseases[result]+".json", 'r', encoding='utf-8') as f:
+    	diagnosis = json.load(f)
+    return diagnosis
+   
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+
+def save_to_disk(uploadedfile, path='.', save_as='default'):
+    extension = os.path.splitext(uploadedfile.filename)[-1]
+    temp_file = os.path.join(path, save_as+extension)
+    with open(temp_file, 'wb') as buffer:
+        shutil.copyfileobj(uploadedfile.file, buffer)
+    return temp_file
